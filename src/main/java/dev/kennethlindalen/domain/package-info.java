303dc8399ec5294924404d037b42af3d2e4bd64c
kennethlindalen <kennethlindalen@gmail.com>
2020-12-26 06:47:17 +0100

    Initial version of ProjPlanner generated by JHipster-6.10.5 with blueprints: vuejs-1.9.2

---
+++ src/main/java/dev/kennethlindalen/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package dev.kennethlindalen.domain;
---
+++ src/main/java/dev/kennethlindalen/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package dev.kennethlindalen.web.websocket.dto;
